f4161fc14702854b48c99f34867816d1930d8c00
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2023-03-17 03:24:33 +0100

    Displaying images

--- src/components/ImageShow.js
+++ src/components/ImageShow.js
@@ -1,5 +1,9 @@
 function ImageShow({ image }) {
-  return <div>ID: {image.alt_description}</div>;
+  return (
+    <div>
+      <img src={image.urls.small_s3} alt={image.alt_description} />
+    </div>
+  );
 }
 
 export default ImageShow;
